0ab691d826b2990044e94604362b429809cd516f
Randy Fay <randy@randyfay.com>
2024-06-09 17:15:22 -0600

    Move items to build into vars

--- build-clients.sh
+++ build-clients.sh
@@ -3,8 +3,11 @@
 set -eu -o pipefail
 set -x
 
-for mysql_version in 8.4.0 8.0.36 5.7.44; do
-  for arch in linux/amd64 linux/arm64; do
+ARCHITECTURES="linux/arm64"
+VERSIONS="8.4.0 8.0.36 5.7.44"
+
+for mysql_version in ${VERSIONS}; do
+  for arch in ${ARCHITECTURES}; do
     if [ ! -d mysql_${mysql_version} ]; then
       curl -L --fail -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-${mysql_version%.?}/mysql-${mysql_version}.tar.gz
       mkdir -p mysql_${mysql_version} && pushd mysql_${mysql_version}
