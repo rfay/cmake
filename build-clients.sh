c071c65ef5e3d39581d7191ddc752bd3478d6b1c
Randy Fay <randy@randyfay.com>
2024-06-08 19:37:03 -0600

    Set up for mounts

--- build-clients.sh
+++ build-clients.sh
@@ -2,7 +2,10 @@
 
 set -eu -o pipefail
 set -x
-curl -L --fail -s -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.0.tar.gz
+curl -L --fail -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.0.tar.gz
 
+mv mysql mysql.bak || true
 mkdir -p mysql && pushd mysql
 tar --strip-components=1 -zxf /tmp/mysql.tgz
+
+docker run --rm -v .:/src randyfay/cmake
