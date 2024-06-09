3c44edc4dd1cf36fef10761e3fef7917ce736514
Randy Fay <randy@randyfay.com>
2024-06-09 17:48:58 -0600

    Account for variety of output dir

--- image/build-mysql-clients.sh
+++ image/build-mysql-clients.sh
@@ -13,9 +13,15 @@ cmake -B${BUILDDIR} -H. -DWITHOUT_SERVER=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/usr/
 cd ${BUILDDIR}
 make mysql mysqldump
 
-# Binaries end up in build/runtime_output_directory
-chmod +x runtime_output_directory/mysql runtime_output_directory/mysqldump
+# mysql8 has runtime_output_directory
+OUTPUTDIR=runtime_output_directory
+if [ ! -d ${OUTPUTDIR} ]; then
+  # mysql 5.7 has "client" for output dir
+  OUTPUTDIR=client
+fi
+# Binaries end up in $OUTPUTDIR
+chmod +x ${OUTPUTDIR}/mysql ${OUTPUTDIR}/mysqldump
 
 # Output version to verify
-runtime_output_directory/mysql --version
-runtime_output_directory/mysqldump --version
+${OUTPUTDIR}/mysql --version
+${OUTPUTDIR}/mysqldump --version
