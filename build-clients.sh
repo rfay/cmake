81e4c1081a13f2f91011505250f97cbe79b1442a
Randy Fay <randy@randyfay.com>
2024-06-09 17:12:59 -0600

    add loop to build the various versions

--- build-clients.sh
+++ build-clients.sh
@@ -2,17 +2,17 @@
 
 set -eu -o pipefail
 set -x
-MYSQL_VERSION=8.4.0
 
-target_arch=linux/amd64
-
-curl -L --fail -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-${MYSQL_VERSION%.?}/mysql-${MYSQL_VERSION}.tar.gz
-
-#mv -f mysql mysql.bak || true
-
-mkdir -p mysql && pushd mysql
-rm -rf build*
-mkdir build
-tar --strip-components=1 -zxf /tmp/mysql.tgz
-
-docker run --rm --platform=${target_arch} -v .:/src randyfay/cmake
+for mysql_version in 8.4.0 8.0.36 5.7.44; do
+  for arch in linux/amd64 linux/arm64; do
+    if [ ! -d mysql_${mysql_version} ]; then
+      curl -L --fail -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-${mysql_version%.?}/mysql-${mysql_version}.tar.gz
+      mkdir -p mysql_${mysql_version} && pushd mysql_${mysql_version}
+      tar --strip-components=1 -zxf /tmp/mysql.tgz
+    fi
+    pushd mysql_${mysql_version}
+    echo "Building ${arch} for mysql ${mysql_version}"
+    docker run --rm --platform=${arch} -v .:/src randyfay/cmake
+    popd
+  done
+done
