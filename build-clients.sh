6f9f3c9bb43c587909bb1fbbf21e6ddce19c3a70
Randy Fay <randy@randyfay.com>
2024-06-09 17:36:58 -0600

    Make dir before using it

--- build-clients.sh
+++ build-clients.sh
@@ -5,13 +5,17 @@ set -x
 
 ARCHITECTURES="linux/arm64"
 VERSIONS="8.4.0 8.0.36 5.7.44"
+VERSIONS="8.0.36 5.7.44"
+
+
+docker pull randyfay/cmake
 
 for mysql_version in ${VERSIONS}; do
   for arch in ${ARCHITECTURES}; do
     if [ ! -d mysql_${mysql_version} ]; then
       curl -L --fail -o /tmp/mysql.tgz https://dev.mysql.com/get/Downloads/MySQL-${mysql_version%.?}/mysql-${mysql_version}.tar.gz
-      mkdir -p mysql_${mysql_version} && pushd mysql_${mysql_version}
-      tar --strip-components=1 -zxf /tmp/mysql.tgz
+      mkdir -p mysql_${mysql_version}
+      tar -C mysql_${mysql_version} --strip-components=1 -zxf /tmp/mysql.tgz
     fi
     pushd mysql_${mysql_version}
     echo "Building ${arch} for mysql ${mysql_version}"
